05daba44d6161f4b54f03b86c2e5396f073ae14c
Chris Liang <ccliang@gmail.com>
2012-06-12 15:11:47 -0500

    Updated model list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   attr_accessible :title, :user_id
   
   belongs_to :user
-  has_many :list_item
-  has_many :items, :through => :list_item
+  has_many :list_items
+  has_many :items, :through => :list_items
   
 end
