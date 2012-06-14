7df23ee4dc0dda142dfca2bef6ca117e73991637
Chris Liang <ccliang@gmail.com>
2012-06-14 18:54:37 -0500

    fixed user routes using resources and except

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,20 @@ MahBucketList::Application.routes.draw do
   
   resources :votes, :only => [:create, :destroy]
   
-  # resources :users
+  get '/users/index' => 'users#new'
+  get '/users/' => 'users#new'
+  
+  resources :users, :except => :index
   
   #recreated routes for users model because wanted the index for users to go to sign up page instead of route error
-  get '/users' => 'users#new'
-  get '/users' => 'users#new'
-  post '/users/create' => 'users#create'
-  get '/users/new' => 'users#new', :as => :new_user
-  get '/users/:user_id/edit' => 'users#edit', :as => :edit_user
-  get '/users/:user_id/show' => 'users#show', :as => :user
-  put 'users/:user_id/update' => 'users#update'
-  delete 'users/:user_id' => 'users#destroy'
+  # get '/users' => 'users#new'
+  # get '/users/index' => 'users#new'
+  # post '/users/create' => 'users#create'
+  # get '/users/new' => 'users#new', :as => :new_user
+  # get '/users/:id/edit' => 'users#edit', :as => :edit_user
+  # get '/users/:id' => 'users#show', :as => :user
+  # put 'users/:user_id/update' => 'users#update'
+  # delete 'users/:user_id' => 'users#destroy'
   
   root to:  'lists#index'
   
