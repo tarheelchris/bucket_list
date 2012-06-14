9622ad73fe54ab29566c811d63f51ce2d852cb2a
Chris Liang <ccliang@gmail.com>
2012-06-14 18:42:08 -0500

    Fixed routes so will not display list of users at users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MahBucketList::Application.routes.draw do
   
   resources :sessions
+  get '/logout' => 'sessions#destroy', :as => :sign_out
   
   resources :lists do
     resources :items
@@ -8,9 +9,17 @@ MahBucketList::Application.routes.draw do
   
   resources :votes, :only => [:create, :destroy]
   
-  resources :users 
+  # resources :users
   
-  get '/logout' => 'sessions#destroy', :as => :sign_out
+  #recreated routes for users model because wanted the index for users to go to sign up page instead of route error
+  get '/users' => 'users#new'
+  get '/users' => 'users#new'
+  post '/users/create' => 'users#create'
+  get '/users/new' => 'users#new', :as => :new_user
+  get '/users/:user_id/edit' => 'users#edit', :as => :edit_user
+  get '/users/:user_id/show' => 'users#show', :as => :user
+  put 'users/:user_id/update' => 'users#update'
+  delete 'users/:user_id' => 'users#destroy'
   
   root to:  'lists#index'
   
